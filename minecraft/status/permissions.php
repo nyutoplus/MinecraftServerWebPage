4ae4f3468452eb6d57d74e8a2de781a155105090
nyutoplus <tttmasapi@gmail.com>
2020-08-20 23:43:59 +0900

    権限ページ作成＆色変更

---
+++ minecraft/status/permissions.php
@@ -0,0 +1,63 @@
+
+<h1 class="content-title">権限について</h1>
+<div style="padding-left: 1rem; padding-bottom: 0.5rem">
+	このサーバーではサーバーの運営を円滑に行えるように下記の権限を設けています。<br>
+</div>
+<table>
+	<tr>
+		<th>権限</th>
+		<th>説明</th>
+	</tr>
+	<tr>
+		<td class="table-all">ALL</td>
+		<td>サーバーに参加したすべてのプレイヤーがここに属します。</td>
+	</tr>
+	<tr>
+		<td class="table-vip">Vip</td>
+		<td>サーバーの維持に関わる貢献をしてくれたプレイヤーがここに属します。<br>一部のプラグインをより柔軟に使用できるようになります。
+		</td>
+	</tr>
+	<tr>
+		<td class="table-adminlv3">Admin_Lv3</td>
+		<td>管理者の見習いがここに属します。<br>管理用のコマンドがいくつか使えるようになります。
+		</td>
+	</tr>
+	<tr>
+		<td class="table-adminlv2">Admin_Lv2</td>
+		<td>通常の管理者はここに属します。<br>管理に必要なコマンドが一通り使えます。
+		</td>
+	</tr>
+	<tr>
+		<td class="table-adminlv1">Admin_Lv1</td>
+		<td>このサーバーのオーナーが属する権限です。<br>コンソールのみ実行可能なコマンド以外すべてのコマンドが使用できます。
+		</td>
+	</tr>
+	<tr>
+		<td class="table-op">OP</td>
+		<td>全てのコマンドが実行できる権限です。</td>
+	</tr>
+</table>
+<div style="padding-top: 1rem">
+	<div class="player-parent table-op">
+		<div class="player-flame">
+			<img src="img/nyuto.png" class="player-image">
+		</div>
+		<div style="padding-left: 1rem">
+			<div class="player-name">nyuto</div>
+			<div style="padding-left: 1rem">
+				このサーバーの発起人＆オーナー<br> Vip権だけ貰ってMinecraft内では悠々とプレイしている
+			</div>
+		</div>
+	</div>
+	<div class="player-parent table-adminlv1">
+		<div class="player-flame">
+			<img src="img/nyutocamera.png" class="player-image">
+		</div>
+		<div style="padding-left: 1rem">
+			<div class="player-name">nyutocamera</div>
+			<div style="padding-left: 1rem">
+				名前で一目瞭然だが上のプレイヤーのサブ垢である。<br> 何かあったときには(多分)駆けつけてくれるであろう・・・
+			</div>
+		</div>
+	</div>
+</div>
