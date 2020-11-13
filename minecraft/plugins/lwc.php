81a54779d0b051b5decaf9f2b2384a811322e0fa
nyutoplus <tttmasapi@gmail.com>
2020-11-13 10:39:39 +0900

    LWC修正

--- minecraft/plugins/lwc.php
+++ minecraft/plugins/lwc.php
@@ -17,7 +17,7 @@
 <div>
 	<div class="cmdof">
 		<div class="cmdtt">
-			<h2 class="cmd"><a id="cinfo" class="anchor"></a>/cinfo</h2>
+			<h2 class="cmd"><a id="cinfo" class="anchor"></a><a href="#cinfo">/cinfo</a></h2>
 			<div class="mark-all">ALL</div>
 		</div>
 		<div>保護の状態が確認できます。</div>
@@ -25,7 +25,7 @@
 	<hr>
 	<div class="cmdof">
 		<div class="cmdtt">
-			<h2 class="cmd"><a id="cprivate" class="anchor"></a>/cprivate [プレイヤー名]...</h2>
+			<h2 class="cmd"><a id="cprivate" class="anchor"></a><a href="#cprivate">/cprivate [プレイヤー名]...</a></h2>
 			<div class="mark-all">ALL</div>
 		</div>
 		<div>自分と指定したプレイヤーのみがアクセス可能となります。<br>指定しない場合は自分のみがアクセス可能となります。</div>
@@ -33,7 +33,7 @@
 	<hr>
 	<div class="cmdof">
 		<div class="cmdtt">
-			<h2 class="cmd"><a id="cmodify" class="anchor"></a>/cmodify [プレイヤー名]</h2>
+			<h2 class="cmd"><a id="cmodify" class="anchor"></a><a href="#cmodify">/cmodify [プレイヤー名]</a></h2>
 			<div class="mark-all">ALL</div>
 		</div>
 		<div>指定したプレイヤーがアクセスできるようになります。</div>
@@ -41,7 +41,7 @@
 	<hr>
 	<div class="cmdof">
 		<div class="cmdtt">
-			<h2 class="cmd"><a id="-cmodify" class="anchor"></a>/cmodify -[プレイヤー名]</h2>
+			<h2 class="cmd"><a id="-cmodify" class="anchor"></a><a href="#-cmodify">/cmodify -[プレイヤー名]</a></h2>
 			<div class="mark-all">ALL</div>
 		</div>
 		<div>指定したプレイヤーがアクセスできないようになります。</div>
@@ -49,7 +49,7 @@
 	<hr>
 	<div class="cmdof">
 		<div class="cmdtt">
-			<h2 class="cmd"><a id="cpublic" class="anchor"></a>/cpublic</h2>
+			<h2 class="cmd"><a id="cpublic" class="anchor"></a><a href="#cpublic">/cpublic</a></h2>
 			<div class="mark-all">ALL</div>
 		</div>
 		<div>全てのプレイヤーがアクセスできるようになります。</div>
@@ -57,7 +57,7 @@
 	<hr>
 	<div class="cmdof">
 		<div class="cmdtt">
-			<h2 class="cmd"><a id="cdonation" class="anchor"></a>/cdonation</h2>
+			<h2 class="cmd"><a id="cdonation" class="anchor"></a><a href="#cdonation">/cdonation</a></h2>
 			<div class="mark-all">ALL</div>
 		</div>
 		<div>誰でもアクセスができますが取り出すことができるのは保護をかけた本人のみです。<br>郵便箱等を作る際に便利です。</div>
@@ -65,7 +65,7 @@
 	<hr>
 	<div class="cmdof">
 		<div class="cmdtt">
-			<h2 class="cmd"><a id="cpassword" class="anchor"></a>/cpassword &lt;パスワード&gt;</h2>
+			<h2 class="cmd"><a id="cpassword" class="anchor"></a><a href="#cpassword">/cpassword &lt;パスワード&gt;</a></h2>
 			<div class="mark-all">ALL</div>
 		</div>
 		<div>パスワードでチェストなどが開けれるようになります。<br>※パスワードを知っていると開けれるので複数人で共有するチェストを作る場合は<a href="#cmodify">/cmodify</a>を使用することをお勧めします。</div>
@@ -73,7 +73,7 @@
 	<hr>
 	<div class="cmdof">
 		<div class="cmdtt">
-			<h2 class="cmd"><a id="climits" class="anchor"></a>/climits</h2>
+			<h2 class="cmd"><a id="climits" class="anchor"></a><a href="#climits">/climits</a></h2>
 			<div class="mark-all">ALL</div>
 		</div>
 		<div>保護しているものの数や保護できる上限を確認します。</div>
@@ -81,7 +81,7 @@
 	<hr>
 	<div class="cmdof">
 		<div class="cmdtt">
-			<h2 class="cmd"><a id="cremove" class="anchor"></a>/cremove</h2>
+			<h2 class="cmd"><a id="cremove" class="anchor"></a><a href="#cremove">/cremove</a></h2>
 			<div class="mark-all">ALL</div>
 		</div>
 		<div>指定した対象の保護を解除します。<br>設定を変更する際はこのコマンドを使用してください。</div>
@@ -89,7 +89,7 @@
 	<hr>
 	<div class="cmdof">
 		<div class="cmdtt">
-			<h2 class="cmd"><a id="cremoveall" class="anchor"></a>/cremoveall</h2>
+			<h2 class="cmd"><a id="cremoveall" class="anchor"></a><a href="#cremoveall">/cremoveall</a></h2>
 			<div class="mark-all">ALL</div>
 		</div>
 		<div>入力した後に適用(<a href="#lwc_confirm">/lwc confirm</a>)すると自分がが保護したものが<mark>全て解除</mark>されます。</div>
@@ -97,7 +97,7 @@
 	<hr>
 	<div class="cmdof">
 		<div class="cmdtt">
-			<h2 class="cmd"><a id="cmagnet" class="anchor"></a>/cmagnet &lt;on | off&gt;<br>/lwc flag magnet &lt;on | off&gt;</h2>
+			<h2 class="cmd"><a id="cmagnet" class="anchor"></a><a href="#cmagnet">/cmagnet &lt;on | off&gt;</a><br><a href="#cmagnet">/lwc flag magnet &lt;on | off&gt;</a></h2>
 			<div class="mark-all">ALL</div>
 		</div>
 		<div>チェストなどのインベントリを持つものをマグネット化します。<br>指定したブロックの周囲に落ちたアイテムを吸引してくれます。</div>
@@ -105,7 +105,7 @@
 	<hr>
 	<div class="cmdof">
 		<div class="cmdtt">
-			<h2 class="cmd"><a id="credstone" class="anchor"></a>/credstone &lt;on | off&gt;<br>/lwc flag redstone &lt;on | off&gt;</h2>
+			<h2 class="cmd"><a id="credstone" class="anchor"></a><a href="#credstone">/credstone &lt;on | off&gt;</a><br><a href="#credstone">/lwc flag redstone &lt;on | off&gt;</a></h2>
 			<div class="mark-all">ALL</div>
 		</div>
 		<div>対象物がレッドストーン入力を受け付けるかどうかを指定します。<br>onにすると対象物がレッドストーン入力を受け付けなくなります。</div>
@@ -113,7 +113,7 @@
 	<hr>
 	<div class="cmdof">
 		<div class="cmdtt">
-			<h2 class="cmd"><a id="cautoclose" class="anchor"></a>/cautoclose &lt;on | off&gt;<br>/lwc flag autoclose &lt;on | off&gt;</h2>
+			<h2 class="cmd"><a id="cautoclose" class="anchor"></a><a href="#cautoclose">/cautoclose &lt;on | off&gt;</a><br><a href="#cautoclose">/lwc flag autoclose &lt;on | off&gt;</a></h2>
 			<div class="mark-all">ALL</div>
 		</div>
 		<div>扉やトラップドアが3秒後に自動的に閉じるようになります。</div>
@@ -121,7 +121,7 @@
 	<hr>
 	<div class="cmdof">
 		<div class="cmdtt">
-			<h2 class="cmd"><a id="callowexplosions" class="anchor"></a>/callowexplosions &lt;on | off&gt;<br>/lwc flag allowexplosions &lt;on | off&gt;</h2>
+			<h2 class="cmd"><a id="callowexplosions" class="anchor"></a><a href="#callowexplosions">/callowexplosions &lt;on | off&gt;</a><br><a href="#callowexplosions">/lwc flag allowexplosions &lt;on | off&gt;</a></h2>
 			<div class="mark-all">ALL</div>
 		</div>
 		<div>対象物が爆発によって破壊されるかを指定できます。</div>
@@ -129,7 +129,7 @@
 	<hr>
 	<div class="cmdof">
 		<div class="cmdtt">
-			<h2 class="cmd"><a id="chopper" class="anchor"></a>/chopper &lt;on | off&gt;<br>/lwc flag hopper &lt;on | off&gt;</h2>
+			<h2 class="cmd"><a id="chopper" class="anchor"></a><a href="#chopper">/chopper &lt;on | off&gt;</a><br><a href="#chopper">/lwc flag hopper &lt;on | off&gt;</a></h2>
 			<div class="mark-all">ALL</div>
 		</div>
 		<div>チェストなどからホッパーでアイテムを取り出せるかを指定します。<br>デフォルトでは盗難を防ぐためオフになっています。</div>
@@ -137,7 +137,7 @@
 	<hr>
 	<div class="cmdof">
 		<div class="cmdtt">
-			<h2 class="cmd"><a id="cpersist" class="anchor"></a>/cpersist<br>/lwc mode persist</h2>
+			<h2 class="cmd"><a id="cpersist" class="anchor"></a><a href="#cpersist">/cpersist</a><br><a href="#cpersist">/lwc mode persist</a></h2>
 			<div class="mark-all">ALL</div>
 		</div>
 		<div>このコマンドは打つたびにオンとオフが切り替わります。<br>デフォルトではオフですが、このコマンドを入力すると保護などが1度のコマンドで複数の対象に使用することができます。</div>
@@ -145,7 +145,7 @@
 	<hr>
 	<div class="cmdof">
 		<div class="cmdtt">
-			<h2 class="cmd"><a id="cdroptransfer" class="anchor"></a>/cdroptransfer &lt;select | on | off | status&gt;<br>/lwc mode droptransfer &lt;select | on | off | status&gt;</h2>
+			<h2 class="cmd"><a id="cdroptransfer" class="anchor"></a><a href="#cdroptransfer">/cdroptransfer &lt;select | on | off | status&gt;</a><br><a href="#cdroptransfer">/lwc mode droptransfer &lt;select | on | off | status&gt;</a></h2>
 			<div class="mark-vip">Vip</div>
 		</div>
 		<div>インベントリからアイテムをドロップすると指定したチェストにアイテムが転送されます。<br>チェストが一杯の場合はプレイヤーに通知され入りきらなかったアイテムは自身に帰ってきます。<br>一度オンにするとオフにするかログアウトするまでオンの状態です。</div>
@@ -153,7 +153,7 @@
 	<hr>
 	<div class="cmdof">
 		<div class="cmdtt">
-			<h2 class="cmd"><a id="lwc" class="anchor"></a>/lwc</h2>
+			<h2 class="cmd"><a id="lwc" class="anchor"></a><a href="#lwc">/lwc</a></h2>
 			<div class="mark-adminlv1">Admin_Lv1</div>
 		</div>
 		<div>ヘルプを表示</div>
@@ -161,7 +161,7 @@
 	<hr>
 	<div class="cmdof">
 		<div class="cmdtt">
-			<h2 class="cmd"><a id="cleanup" class="anchor"></a>/lwc admin cleanup</h2>
+			<h2 class="cmd"><a id="cleanup" class="anchor"></a><a href="#cleanup">/lwc admin cleanup</a></h2>
 			<div class="mark-adminlv1">Admin_Lv1</div>
 		</div>
 		<div>LWCでは座標で保護を行うため存在していないブロックに対して保護を行っているものもあります。<br>このコマンドはそれを確認して削除するものです。</div>
@@ -169,7 +169,7 @@
 	<hr>
 	<div class="cmdof">
 		<div class="cmdtt">
-			<h2 class="cmd"><a id="expire" class="anchor"></a>/lwc admin expire &lt;時間&gt;</h2>
+			<h2 class="cmd"><a id="expire" class="anchor"></a><a href="#expire">/lwc admin expire &lt;時間&gt;</a></h2>
 			<div class="mark-adminlv1">Admin_Lv1</div>
 		</div>
 		<div>指定した期間アクセスがない保護を削除します。<br>例)/lwc admin expire 1 month</div>
@@ -177,7 +177,7 @@
 	<hr>
 	<div class="cmdof">
 		<div class="cmdtt">
-			<h2 class="cmd"><a id="find" class="anchor"></a>/lwc admin find &lt;プレイヤー&gt; [ページ]</h2>
+			<h2 class="cmd"><a id="find" class="anchor"></a><a href="#find">/lwc admin find &lt;プレイヤー&gt; [ページ]</a></h2>
 			<div class="mark-adminlv1">Admin_Lv1</div>
 		</div>
 		<div>指定したプレイヤーが作成した保護一覧を表示します。</div>
@@ -185,7 +185,7 @@
 	<hr>
 	<div class="cmdof">
 		<div class="cmdtt">
-			<h2 class="cmd"><a id="forceowner" class="anchor"></a>/lwc admin forceowner &lt;プレイヤー&gt; [ProtectionID]</h2>
+			<h2 class="cmd"><a id="forceowner" class="anchor"></a><a href="#forceowner">/lwc admin forceowner &lt;プレイヤー&gt; [ProtectionID]</a></h2>
 			<div class="mark-adminlv1">Admin_Lv1</div>
 		</div>
 		<div>指定したプレイヤーに保護の所有権が移ります。<br>IDを指定した場合はそのIDに対応する保護の所有権が移ります。</div>
@@ -193,7 +193,7 @@
 	<hr>
 	<div class="cmdof">
 		<div class="cmdtt">
-			<h2 class="cmd"><a id="remove" class="anchor"></a>/lwc admin remove &lt;ProtectionID&gt;</h2>
+			<h2 class="cmd"><a id="remove" class="anchor"></a><a href="#remove">/lwc admin remove &lt;ProtectionID&gt;</a></h2>
 			<div class="mark-adminlv1">Admin_Lv1</div>
 		</div>
 		<div>指定したIDの保護が解除されます。</div>
@@ -201,7 +201,7 @@
 	<hr>
 	<div class="cmdof">
 		<div class="cmdtt">
-			<h2 class="cmd"><a id="purge" class="anchor"></a>/lwc admin purge &lt;プレイヤー&gt;</h2>
+			<h2 class="cmd"><a id="purge" class="anchor"></a><a href="#purge">/lwc admin purge &lt;プレイヤー&gt;</a></h2>
 			<div class="mark-adminlv1">Admin_Lv1</div>
 		</div>
 		<div>指定したプレイヤーが保護したものが全て解除されます。</div>
@@ -209,7 +209,7 @@
 	<hr>
 	<div class="cmdof">
 		<div class="cmdtt">
-			<h2 class="cmd"><a id="cleanup" class="anchor"></a>/lwc admin cleanup</h2>
+			<h2 class="cmd"><a id="cleanup" class="anchor"></a><a href="#cleanup">/lwc admin cleanup</a></h2>
 			<div class="mark-adminlv1">Admin_Lv1</div>
 		</div>
 		<div>データベースがクリーンアップされます。</div>
@@ -217,7 +217,7 @@
 	<hr>
 	<div class="cmdof">
 		<div class="cmdtt">
-			<h2 class="cmd"><a id="report" class="anchor"></a>/lwc admin report</h2>
+			<h2 class="cmd"><a id="report" class="anchor"></a><a href="#report">/lwc admin report</a></h2>
 			<div class="mark-adminlv1">Admin_Lv1</div>
 		</div>
 		<div>LWCのパフォーマンスレポートを表示します。</div>
