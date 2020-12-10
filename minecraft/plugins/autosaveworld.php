44a579d1a1bb9a56301fcf70066fb9deb7be94d0
nyutoplus <tttmasapi@gmail.com>
2020-12-10 15:21:48 +0900

    AutoSaveWorldコマンド記載

--- minecraft/plugins/autosaveworld.php
+++ minecraft/plugins/autosaveworld.php
@@ -1,4 +1,3 @@
-
 <h1 class="content-title">
 	<a class="hide-line" href="https://dev.bukkit.org/projects/autosaveworld">AutoSaveWorld</a>の使用可能コマンド
 </h1>
@@ -9,5 +8,205 @@
 	</ul>
 	<br> <strong>[]</strong>で括ってある部分は省略可能です。<br> <strong>&lt;&gt;</strong>で括ってある部分は省略できないので引数を入力してください。<br> <strong>&lt;A|B|C&gt;</strong>で括ってある場合はA～Cのいずれかを入力してください。
 </div>
-<div style="height: 1rem"></div>
-<div>作成中・・・</div>
+<hr>
+<div>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd"><a id="aswinfo" class="anchor"></a><a href="#aswinfo">/asw</a><br><a href="#aswinfo">/asw info</a></h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div>AutoSaveWorldのコマンドです。</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd"><a id="aswhelp" class="anchor"></a><a href="#aswhelp">/asw help</a></h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div>AutoSaveWorldのコマンド一覧を表示します。</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd"><a id="aswsave" class="anchor"></a><a href="#aswsave">/save</a><br><a href="#aswsave">/asw save</a></h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div>サーバーデータを保存します。</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd"><a id="aswbackup" class="anchor"></a><a href="#aswbackup">/backup</a><br><a href="#aswbackup">/asw backup</a></h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div>サーバーのバックアップを取ります。</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd"><a id="aswpurge" class="anchor"></a><a href="#aswpurge">/purge</a><br><a href="#aswpurge">/asw purge</a></h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div>アクティブでないプレイヤーからプラグイン情報をパージします。</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd"><a id="aswrestart" class="anchor"></a><a href="#aswrestart">/asw restart</a></h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div>サーバーを再起動します。</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd"><a id="aswforcerestart" class="anchor"></a><a href="#aswforcerestart">/asw forcerestart</a></h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div>カウントダウン無しでサーバーを再起動します。</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd"><a id="aswregenworld" class="anchor"></a><a href="#aswregenworld">/asw regenworld &lt;ワールド名&gt; &lt;ワールドのリージョンフォルダー&gt;</a></h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div>ワールドを再生成します。</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd"><a id="aswpmanagerload" class="anchor"></a><a href="#aswpmanagerload">/asw pmanager load &lt;プラグイン名&gt;</a></h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div>指定したプラグインをロードします。</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd"><a id="aswpmanagerunload" class="anchor"></a><a href="#aswpmanagerunload">/asw pmanager unload &lt;プラグイン名&gt;</a></h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div>指定したプラグインをアンロードします。</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd"><a id="aswpmanagerreload" class="anchor"></a><a href="#aswpmanagerreload">/asw pmanager reload &lt;プラグイン名&gt;</a></h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div>指定したプラグインをリロードします。</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd"><a id="aswprocessstart" class="anchor"></a><a href="#aswprocessstart">/asw process start &lt;プロセス名&gt; &lt;コマンドライン&gt;</a></h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div>コマンドラインを使用してプロセスを開始します。</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd"><a id="aswprocessstop" class="anchor"></a><a href="#aswprocessstop">/asw process stop &lt;プロセス名&gt;</a></h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div>指定したプロセスを終了します。</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd"><a id="aswprocessoutput" class="anchor"></a><a href="#aswprocessoutput">/asw process output &lt;プロセス名&gt;</a></h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div>指定したプロセスの最終出力を表示します。</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd"><a id="aswprocessinput" class="anchor"></a><a href="#aswprocessinput">/asw process input &lt;プロセス名&gt; &lt;入力&gt;</a></h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div>指定したプロセスに入力します。</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd"><a id="aswprocesslist" class="anchor"></a><a href="#aswprocesslist">/asw process list</a></h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div>実行しているプロセス一覧を表示します。</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd"><a id="aswserverstatus" class="anchor"></a><a href="#aswserverstatus">/asw serverstatus</a></h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div>CPU、メモリ、HDDの使用量を表示します。</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd"><a id="aswstop" class="anchor"></a><a href="#aswstop">/asw stop</a></h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div>サーバーを停止します。</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd"><a id="aswforcegc" class="anchor"></a><a href="#aswforcegc">/asw forcegc</a></h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div>強制的にガベージコレクションをします。</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd"><a id="aswreload" class="anchor"></a><a href="#aswreload">/asw reload</a></h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div>全てのコンフィグをリロードします。</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd"><a id="aswreloadconfig" class="anchor"></a><a href="#aswreloadconfig">/asw reloadconfig</a></h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div>コンフィグをリロードします。</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd"><a id="aswreloadmsg" class="anchor"></a><a href="#aswreloadmsg">/asw reloadmsg</a></h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div>メッセージをリロードします。</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd"><a id="aswlocaleavailable" class="anchor"></a><a href="#aswlocaleavailable">/asw locale available</a></h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div>利用可能な言語を表示します。</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd"><a id="aswlocaleload" class="anchor"></a><a href="#aswlocaleload">/asw locale load &lt;言語&gt;</a></h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div>指定した言語を読み込みます。</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd"><a id="aswversion" class="anchor"></a><a href="#aswversion">/asw version</a></h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div>AutoSaveWorldのバージョンを表示します。</div>
+	</div>
+</div>
