431b342a9b20869a1ad6c29f2e38c0ef251ac640
nyutoplus <tttmasapi@gmail.com>
2020-12-18 02:04:55 +0900

    ルール、トラップタワー判定の改定

--- minecraft/rules/rules.php
+++ minecraft/rules/rules.php
@@ -1,172 +1,170 @@
 <h1 class="content-title">サーバールール</h1>
 <p>
-	サーバーに参加するプレイヤーの皆様(以下、プレイヤー)は当サーバーに参加した時点で本ルールに同意したものとします。<br> 本ルールはサーバー内の情勢、運営の都合で予告なく変更されることがあります。変更した際には<a class="hide-line" href="https://discord.gg/wshGjfg">Discordサーバー</a>にて通知しますので是非サーバーに参加してください。<br>
+	サーバーに参加するプレイヤーの皆様(以下、プレイヤー)は当サーバーに参加した時点で本ルールに同意したものとします。<br>
+	本ルールはサーバー内の情勢、運営の都合で予告なく変更されることがあります。変更した際には<a class="hide-line" href="https://discord.gg/wshGjfg">Discordサーバー</a>にて通知しますので是非サーバーに参加してください。<br>
 </p>
 <hr>
 <p>
-<h2 class="content-subtitle">第1条 サーバーの禁止事項</h2>
-<h3 style="font-size: 1.3rem; margin-left: 1rem">第1項 他人のプレイに支障をきたす行為</h3>
-<ul style="margin-left: 1rem;">
-	<li>・公共設備の破壊等
-
-	<li>・他プレイヤー、企業、団体への誹謗中傷、暴言、脅迫等
-
-	<li>・過剰な要求、命令、宣伝等
-
-	<li>・他プレイヤーの迷惑となるチャット発言(過度な論争行為等)
-
-	<li>・スパム行為(連続発言等)
-
-	<li>・他プレイヤーのプレイ妨害に繋がる行為(ストーカー等)
-
-	<li>・他プレイヤーの個人情報の無断開示する行為等
-
-	<li>・以下に当てはまる建築物の作成、スキンの使用、発言などの表現行為
-		<ul style="margin-left: 1rem;">
-			<li>1.わいせつ、又は残虐な物(エロ、グロ系)
-
-			<li>2.反社会的な政治や宗教思想を示唆するもの
-
-		</ul>
-
-	<li>・詐欺行為(なりすまし、取引の詐称等)
-
-	<li>・ルール違反に過度に反応する行為、又は助長する行為
-
-	<li>・当サーバー及び関連サービスに正規以外の方法で接続する、又は接続させる行為
-
-</ul>
+<h2 class="content-subtitle"><strong><a id="1" class="anchor"></a><a href="#1">第1条</a> サーバーの禁止事項</strong></h2>
+<h3 style="margin-left: 0.5rem"><strong><a id="1-1" class="anchor"></a><a href="#1-1">第1項</a> 他人のプレイに支障をきたす行為</strong></h3>
+<div style="margin-left: 1rem">
+	<a id="1-1-1" class="anchor"></a><a href="#1-1-1">1.</a>公共設備の破壊等(過失を除く)[10点]<br>
+	<a id="1-1-2" class="anchor"></a><a href="#1-1-2">2.</a>他プレイヤー、企業、団体への誹謗中傷、暴言、脅迫等[30点]<br>
+	<a id="1-1-3" class="anchor"></a><a href="#1-1-3">3.</a>過剰な要求、命令、宣伝等[10点]<br>
+	<a id="1-1-4" class="anchor"></a><a href="#1-1-4">4.</a>他プレイヤーの迷惑となるチャット発言(過度な論争行為等)[10点]<br>
+	<a id="1-1-5" class="anchor"></a><a href="#1-1-5">5.</a>スパム行為(連続発言等)[30点]<br>
+	<a id="1-1-6" class="anchor"></a><a href="#1-1-6">6.</a>他プレイヤーのプレイ妨害に繋がる行為(ストーカー等)[20点]<br>
+	<a id="1-1-7" class="anchor"></a><a href="#1-1-7">7.</a>他プレイヤーの個人情報の無断開示する行為等[50点]<br>
+	<a id="1-1-8" class="anchor"></a><a href="#1-1-8">8.</a>以下に当てはまる建築物の作成、スキンの使用、発言などの表現行為[10点]<br>
+	<div style="margin-left: 1rem;">
+		<a id="1-1-8-1" class="anchor"></a><a href="#1-1-8-1">1.</a>わいせつ、又は残虐な物(エロ、グロ系)<br>
+		<a id="1-1-8-2" class="anchor"></a><a href="#1-1-8-2">2.</a>反社会的な政治や宗教思想を示唆するもの<br>
+	</div>
+	<a id="1-1-9" class="anchor"></a><a href="#1-1-9">9.</a>詐欺行為(なりすまし、取引の詐称等)[50点]<br>
+	<a id="1-1-10" class="anchor"></a><a href="#1-1-10">10.</a>ルール違反に過度に反応する行為、又は助長する行為[10点]<br>
+	<a id="1-1-11" class="anchor"></a><a href="#1-1-11">11.</a>当サーバー及び関連サービスに正規以外の方法で接続する、又は接続させる行為[50点]
+</div>
 <br>
-<h3 style="font-size: 1.3rem; margin-left: 1rem">第2項 不正行為</h3>
-<ul style="margin-left: 1rem">
-	<li>・下記機能のあるMOD、リソースパック、ツール等の使用及び使用を示唆する行為
-		<ul style="margin-left: 1rem">
-			<li>1.ブロックを透過させる機能のあるもの
-
-			<li>2.鉱石の位置を特定する機能のあるもの
-
-			<li>3.他人から識別不能になる機能のあるもの
-
-			<li>4.自動的に採掘や整地、作物の収穫などを可能にするもの
-
-		</ul>
-
-	<li>・自動連打や自動操作若しくはチート行為を可能にする外部ツールの使用及び使用を示唆する行為
-
-	<li>・ゲーム内不具合を悪用若しくは流布する行為(BUD回路等の一部の不具合を除く)
-
-	<li>・正規ルート以外で入手したMinecraftアカウントを使用して当サーバーに接続する行為
-
-	<li>・Proxy、VPNを経由して当サーバーに接続する行為
-
-</ul>
+<h3 style="margin-left: 0.5rem"><strong><a id="1-2" class="anchor"></a><a href="#1-2">第2項</a> 不正行為</strong></h3>
+<div style="margin-left: 1rem">
+	<a id="1-2-1" class="anchor"></a><a href="#1-2-1">1.</a>下記機能のあるMOD、リソースパック、ツール等の使用及び使用を示唆する行為[50点]<br>
+	<div style="margin-left: 1rem">
+		<a id="1-2-1-1" class="anchor"></a><a href="#1-2-1-1">1.</a>ブロックを透過させる機能のあるもの<br>
+		<a id="1-2-1-2" class="anchor"></a><a href="#1-2-1-2">2.</a>鉱石の位置を特定する機能のあるもの<br>
+		<a id="1-2-1-3" class="anchor"></a><a href="#1-2-1-3">3.</a>他人から識別不能になる機能のあるもの<br>
+		<a id="1-2-1-4" class="anchor"></a><a href="#1-2-1-4">4.</a>自動的に採掘や整地、作物の収穫などを可能にするもの<br>
+	</div>
+	<a id="1-2-2" class="anchor"></a><a href="#1-2-2">2.</a>自動連打や自動操作若しくはチート行為を可能にする外部ツールの使用及び使用を示唆する行為<br>(自動連打については毎秒1回までであれば許容します。)[50点]<br>
+	<a id="1-2-3" class="anchor"></a><a href="#1-2-3">3.</a>ゲーム内不具合を悪用若しくは流布する行為(BUD回路、無限増殖バグは除きます。)[50点]<br>
+	<a id="1-2-4" class="anchor"></a><a href="#1-2-4">4.</a>正規ルート以外で入手したMinecraftアカウントを使用して当サーバーに接続する行為[50点]<br>
+	<a id="1-2-5" class="anchor"></a><a href="#1-2-5">5.</a>Proxy、VPNを経由して当サーバーに接続する行為[50点]
+</div>
 <br>
-<h3 style="font-size: 1.3rem; margin-left: 1rem">第3項 異常負荷をかける行為</h3>
-<ul style="margin-left: 1rem">
-	<li>・ON,OFFスイッチのない高周波(クロック周期が4RSチック以下(リピーター最大遅延1つ分))のクロック回路を作成若しくはクロック周期が1秒未満のクロック回路を未使用時にON状態のまま放置する行為
-
-	<li>・中規模以上のトラップタワー(基準は<a class="hide-line" href="?category=rules&content=traptower-rules">こちら</a>にて)を公共ポータル等の公共交通機関の16チャンク以内に設置する行為
-
-	<li>・中規模以上の回路(基準は<a class="hide-line" href="?category=rules&content=traptower-rules">こちら</a>にて)を公共ポータル等の公共交通機関の16チャンク以内に設置する行為
-
-	<li>・周辺のALL値が200を超えるような施設の建設(トラップタワーを除く)
-
-	<li>・停止機構の無い村人などの無限増殖装置の作成
-
-</ul>
+<h3 style="margin-left: 0.5rem"><strong><a id="1-3" class="anchor"></a><a href="#1-3">第3項</a> サーバー、クライアントに異常負荷をかける行為</strong></h3>
+<div style="margin-left: 1rem">
+	<a id="1-3-1" class="anchor"></a><a href="#1-3-1">1.</a>ON,OFFスイッチのない高周波(クロック周期が4RSチック以下(リピーター最大遅延1つ分))のクロック回路を作成若しくはクロック周期が1秒未満のクロック回路を未使用時にON状態のまま放置する行為[5点]<br>
+	<a id="1-3-2" class="anchor"></a><a href="#1-3-2">2.</a>中規模以上のトラップタワー(基準は<a class="hide-line" href="?category=rules&content=traptower-rules">こちら</a>にて)を公共ポータル等の公共交通機関の8チャンク以内に設置する行為[5点]<br>
+	<a id="1-3-3" class="anchor"></a><a href="#1-3-3">3.</a>中規模以上の回路(基準は<a class="hide-line" href="?category=rules&content=traptower-rules">こちら</a>にて)を公共ポータル等の公共交通機関の8チャンク以内に設置する行為[5点]<br>
+	<a id="1-3-4" class="anchor"></a><a href="#1-3-4">4.</a>周辺のALL値が200を超えるような施設の建設(トラップタワーを除く)[5点]<br>
+	<a id="1-3-5" class="anchor"></a><a href="#1-3-5">5.</a>停止機構の無い村人などの無限増殖装置の作成[5点]
+</div>
 <br>
-<h3 style="font-size: 1.3rem; margin-left: 1rem">第4項 その他</h3>
-<ul style="margin-left: 1rem">
-	<li>・運営チームからの指示に従わない行為(明らかに不当な指示は除く)
-
-	<li>・運営チームが不適切であると判断した行為
-
-</ul>
+<h3 style="margin-left: 0.5rem"><strong><a id="1-4" class="anchor"></a><a href="#1-4">第4項</a> その他</strong></h3>
+<div style="margin-left: 1rem">
+	<a id="1-4-1" class="anchor"></a><a href="#1-4-1">1.</a>運営チームからの指示に従わない行為(明らかに不当な指示は除く)[5点]<br>
+	<a id="1-4-2" class="anchor"></a><a href="#1-4-2">2.</a>運営チームが不適切であると判断した行為[5点]
+</div>
 <br>
-<h2 class="content-subtitle">第2条 サーバーにおける建築と保護のルール</h2>
-<h3 style="font-size: 1.3rem; margin-left: 1rem">第1項 保護の設定</h3>
-<ul style="margin-left: 1rem">
-	<li>・初期スポーンワールド、公共交通機関以外は建築、及びWorldGuardでの保護が可能です。
-
-	<li>(詳しい保護方法は<a class="hide-line" href="?category=plugins&content=worldguard">こちら</a>にて解説します。)
-
-</ul>
+<h2 class="content-subtitle"><strong><a id="2" class="anchor"></a><a href="#2">第2条</a> サーバーにおける建築と保護のルール</strong></h2>
+<h3 style="margin-left: 0.5rem"><strong><a id="2-1" class="anchor"></a><a href="#2-1">第1項</a> 保護の設定</strong></h3>
+<div style="margin-left: 1rem">
+	<a id="2-1-1" class="anchor"></a><a href="#2-1-1">1.</a>初期スポーンワールド、公共交通機関以外は建築、及びWorldGuardでの保護が可能です。<br>
+	(詳しい保護方法は<a class="hide-line" href="?category=plugins&content=worldguard">こちら</a>にて解説します。)
+</div>
 <br>
-<h3 style="font-size: 1.3rem; margin-left: 1rem">第2項 禁止事項</h3>
-<ul style="margin-left: 1rem">
-	<li>・公共設備(道路等)を保護範囲に指定すること、及び破壊すること
-
-	<li>・他プレイヤーの建築物を保護範囲に指定すること、及び破壊すること
-
-	<li>・他プレイヤーへの嫌がらせを目的とした保護の作成行為、及び建築、破壊行為
-
-</ul>
+<h3 style="margin-left: 0.5rem"><strong><a id="2-2" class="anchor"></a><a href="#2-2">第2項</a> 禁止事項</strong></h3>
+<div style="margin-left: 1rem">
+	<a id="2-2-1" class="anchor"></a><a href="#2-2-1">1.</a>公共設備(道路、ポータル等)を保護範囲に指定すること、及び破壊すること[10点]<br>
+	<a id="2-2-2" class="anchor"></a><a href="#2-2-2">2.</a>他プレイヤーの建築物を保護範囲に指定すること、及び破壊すること[10点]<br>
+	<a id="2-2-3" class="anchor"></a><a href="#2-2-3">3.</a>資源採掘のための保護を設定すること[10点]<br>
+	<a id="2-2-4" class="anchor"></a><a href="#2-2-4">4.</a>他プレイヤーへの嫌がらせを目的とした保護の作成行為、及び建築、破壊行為[10点]
+</div>
 <br>
-<h3 style="font-size: 1.3rem; margin-left: 1rem">第3項 保護の強制解除、建築物の強制移動</h3>
+<h3 style="margin-left: 0.5rem"><strong><a id="2-3" class="anchor"></a><a href="#2-3">第3項</a> 保護の強制解除、建築物の強制移動</strong></h3>
 <mark style="margin-left: 1rem">以下に該当する保護、建築物は移動、解除、撤去する可能性があります。</mark>
-<ul style="margin-left: 1rem">
-	<li>・本ルールの他の事項に抵触する保護
-
-	<li>・建築途中、又は未建築で30日以上プレイヤーログインが無い保護<br>(完成品か否かは運営が独断で判断します。)
-
-	<li>・公共施設を建築する際の妨げとなる建築物
-
-	<li>・オーナーがBAN処分を受けたプレイヤーのみである保護
-
-	<li>・周辺のALL値が200を超える原因となる保護
-
-	<li>・著しく景観を損なう創作物のある保護
-
-	<li>・公序良俗に反する、若しくは反社会的な建築物
-
-	<li>・無意味、又は不必要な保護であると運営チームが判断した場合
-
-	<li>・他社の著作権、肖像権を侵害する建築物
-
-</ul>
-<br>
-<h2 class="content-subtitle">第3条 整地のルール</h2>
-<h3 style="font-size: 1.3rem; margin-left: 1rem">第1項 整地する際のルール</h3>
-<ul style="margin-left: 1rem">
-	<li>・整地をする際は下から掘らず、上から掘ってください。<br>(下から掘るとそれを知らずに上から掘ってきた人が落下死する可能性があります。)
-
-	<li>・他人の建築物の下を掘らない
-
-</ul>
-<br>
-<h2 class="content-subtitle">第4条 処罰の執行及び異議の申し立て</h2>
-<h3 style="font-size: 1.3rem; margin-left: 1rem">第1項 処罰の執行</h3>
-<ul style="margin-left: 1rem">
-	<li>・本ルールに抵触したプレイヤーを発見した場合、度合いに応じて運営が処罰<mark>(警告、無期限BAN)</mark>を実施します。
-
-	<li>・本ルールに抵触しない場合でも運営チームが不適切であると判断した場合は運営チームの合議によって処罰<mark>(警告、無期限BAN)</mark>を実施する可能性があります。
-
-</ul>
-<br>
-<h3 style="font-size: 1.3rem; margin-left: 1rem">第2項 異議申し立て</h3>
 <div style="margin-left: 1rem">
-	BAN処分されたプレイヤーは<a class="hide-line" href="?category=others&content=contactus">こちら</a>より運営チームに異議申し立てを行うことができますが、不当な処罰である場合を除き、BANの解除は一切行いません。<br> 又、以下に当てはまる場合は如何なる理由でも異議申し立ては受理されません。
-</div>
-<ul style="margin-left: 1rem">
-	<li>・被処罰者本人以外から投稿された異議申し立て
-
-	<li>・執行から60日以上経過した処罰
-
-</ul>
-<br>
-<h2 class="content-subtitle">第5条 動画投稿、生放送について</h2>
-<h3 style="font-size:1.3rem; margin-left: 1rem">第1項 動画投稿、生放送する際の注意</h3>
-<ul style="margin-left: 1rem">
-	<li>・動画投稿、生放送をされる際は他プレイヤーの迷惑にならないように注意してください。
-	<li>・動画投稿、生放送をする際も本ルールは適用されます。例外は一切ありませんのでご注意下さい。
-	<li>・動画、生放送の視聴者をこのサーバーに招待するのはご自由ですが、参加する前に本ルールを読むよう伝えておいてください。
-	<li>・動画投稿、生放送をされた際に起こった不具合、損失などのトラブルには運営チームは一切関与しません。当事者で解決するようにしてください。
-</ul>
-<br>
-<h2 class="content-subtitle">第6条 免責事項</h2>
-<div style="margin-left: 1rem">
-	プレイヤーの皆様に安心してプレイしていただけるよう、運営チームがゲーム内チャットメッセージや行動ログ等を確認する場合があります。<br> プレイヤー同士のトラブルへは、ルール上処罰されるべき行為を確認できた場合を除き運営チームは一切関与しません。プレイヤー間での解決に努めてください。<br> プレイヤー間での取引に関しては自己の責任において行ってください。損失やトラブルが発生しても運営チームは一切関与、補償致しません。<br> ルール違反による処置、プレイヤー間のトラブル、ゲーム内不具合、プレイヤーの過失によって発生した損失の補填は一切ありませんのでご了承ください。<br> サーバーメンテナンス時に強制的に切断されたことによる損失などの補填は一切ありませんのでご了承ください。
+	<a id="2-3-1" class="anchor"></a><a href="#2-3-1">1.</a>本ルールの他の事項に抵触する保護<br>
+	<a id="2-3-2" class="anchor"></a><a href="#2-3-2">2.</a>建築途中、又は未建築で30日以上プレイヤーログインが無い保護<br>(完成品か否かは運営が独断で判断します。)<br>
+	<a id="2-3-3" class="anchor"></a><a href="#2-3-3">3.</a>公共施設を建築する際の妨げとなる建築物<br>
+	<a id="2-3-4" class="anchor"></a><a href="#2-3-4">4.</a>オーナーがBAN処分を受けたプレイヤーのみである保護<br>
+	<a id="2-3-5" class="anchor"></a><a href="#2-3-5">5.</a>周辺のALL値が200を超える原因となる保護<br>
+	<a id="2-3-6" class="anchor"></a><a href="#2-3-6">6.</a>著しく景観を損なう創作物のある保護<br>
+	<a id="2-3-7" class="anchor"></a><a href="#2-3-7">7.</a>公序良俗に反する、若しくは反社会的な建築物<br>
+	<a id="2-3-8" class="anchor"></a><a href="#2-3-8">8.</a>無意味、又は不必要な保護であると運営チームが判断した場合<br>
+	<a id="2-3-9" class="anchor"></a><a href="#2-3-9">9.</a>他社の著作権、肖像権を侵害する建築物
 </div>
 <br>
-<time>2020年9月10日</time>
-改定
+<h2 class="content-subtitle"><strong><a id="3" class="anchor"></a><a href="#3">第3条</a> 整地のルール</strong></h2>
+<h3 style="margin-left: 0.5rem"><strong><a id="3-1" class="anchor"></a><a href="#3-1">第1項</a> 整地する際のルール</strong></h3>
+<div style="margin-left: 1rem">
+	<a id="3-1-1" class="anchor"></a><a href="#3-1-1">1.</a>整地をする際は下から掘らず、上から掘ってください。<br>(下から掘るとそれを知らずに上から掘ってきた人が落下死する可能性があります。)<br>
+	<a id="3-1-2" class="anchor"></a><a href="#3-1-2">2.</a>他人の建築物の下を掘らない<br>
+	<a id="3-1-3" class="anchor"></a><a href="#3-1-3">3.</a>整地
+</div>
+<br>
+<h2 class="content-subtitle"><strong><a id="4" class="anchor"></a><a href="#4">第4条</a> 処罰の執行及び異議の申し立て</strong></h2>
+<h3 style="margin-left: 0.5rem"><strong><a id="4-1" class="anchor"></a><a href="#4-1">第1項</a> 処罰の執行</strong></h3>
+<div style="margin-left: 1rem">
+	<a id="4-1-1" class="anchor"></a><a href="#4-1-1">1.</a>本ルールに抵触したプレイヤーを発見した場合、度合いに応じて運営が処罰<mark>(警告、無期限BAN)</mark>を実施します。<br>
+		処罰内容に関しては<a href="#4-1-3">こちら</a>に基づくものとします。<br>
+	<a id="4-1-2" class="anchor"></a><a href="#4-1-2">2.</a>本ルールに抵触しない場合でも運営チームが不適切であると判断した場合は運営チームの合議によって処罰<mark>(警告、無期限BAN)</mark>を実施する可能性があります。<br>
+		処罰内容に関しては<a href="#4-1-3">こちら</a>に基づくものとします。<br>
+	<a id="4-1-3" class="anchor"></a><a href="#4-1-3">3.</a>処罰執行は各ルールの末尾に記載している点数を元に行います。<br>
+		<div style="margin-left:0.5rem">
+			<a id="4-1-3-1" class="anchor"></a><a href="#4-1-3-1">1.</a>処罰はルールに抵触するたび点数を加算し、各処罰を実行します。<br>
+			<a id="4-1-3-2" class="anchor"></a><a href="#4-1-3-2">2.</a>ルールの点数が10点以下の場合、運営の指示に従えば5点の減点が行われます。<br>
+				また、原点が行われた後、再度同様の違反をしたりするなど、反省が見られない場合は減点は無かったものとし、追加で犯したルールの点数が加算されます。<br>
+			<a id="4-1-3-3" class="anchor"></a><a href="#4-1-3-3">3.</a>処罰の内容は累計点数を元にして、下記の通り行われます。<br>
+			<div style="margin-left: 0.5rem">
+				<strong>累計点数が20点未満の場合、警告を行います。<br>
+				累計点数が20点を超える場合、1週間の本サーバーへの接続禁止とさせていただきます。<br>
+				累計点数が40点を超える場合、警告なしでBANを実行させていただきます。<br>
+				<mark>※BANを実行した場合、そのプレイヤーが本サーバーで行った行動すべてがロールバックされます。<br>
+				また、その結果は<a href="#4-2">下記</a>の異議申し立てが受理されたとしても覆らないものとします。</mark></strong>
+			</div>
+		</div>
+</div>
+<br>
+<h3 style="margin-left: 0.5rem"><strong><a id="4-2" class="anchor"></a><a href="#4-2">第2項</a> 異議申し立て</strong></h3>
+<div style="margin-left: 1rem">
+	<a id="4-2-1" class="anchor"></a><a href="#4-2-1">1.</a>BAN処分されたプレイヤーは<a class="hide-line" href="?category=others&content=contactus">こちら</a>
+	より運営チームに異議申し立てを行うことができますが、不当な処罰である場合を除き、BANの解除は一切行いません。<br>
+	 又、以下に当てはまる場合は如何なる理由でも異議申し立ては受理されません。
+</div>
+<br>
+<div style="margin-left: 1rem">
+	<a id="4-2-1-1" class="anchor"></a><a href="#4-2-1-1">1.</a>被処罰者本人以外から投稿された異議申し立て<br>
+	<a id="4-2-1-2" class="anchor"></a><a href="#4-2-1-2">2.</a>執行から60日以上経過した処罰
+</div>
+<br>
+<h2 class="content-subtitle"><strong><a id="5" class="anchor"></a><a href="#5">第5条</a> 動画投稿、生放送について</strong></h2>
+<h3 style="margin-left: 0.5rem"><strong><a id="5-1" class="anchor"></a><a href="#5-1">第1項</a> 動画投稿、生放送する際の注意</strong></h3>
+<div style="margin-left: 1rem">
+	<a id="5-1-1" class="anchor"></a><a href="#5-1-1">1.</a>動画投稿、生放送をされる際は他プレイヤーの迷惑にならないように注意してください。<br>
+	<a id="5-1-2" class="anchor"></a><a href="#5-1-2">2.</a>動画投稿、生放送をする際も本ルールは適用されます。例外は一切ありませんのでご注意下さい。<br>
+	<a id="5-1-3" class="anchor"></a><a href="#5-1-3">3.</a>動画、生放送の視聴者をこのサーバーに招待するのはご自由ですが、参加する前に本ルールを読むよう伝えておいてください。<br>
+	<a id="5-1-4" class="anchor"></a><a href="#5-1-4">4.</a>動画投稿、生放送をされた際に起こった不具合、損失などのトラブルには運営チームは一切関与しません。当事者で解決するようにしてください。<br>
+</div>
+<br>
+<h2 class="content-subtitle"><strong><a id="6" class="anchor"></a><a href="#6">第6条</a> 寄付について</strong></h2>
+<h3 style="margin-left: 0.5rem"><strong><a id="6-1" class="anchor"></a><a href="#6-1">第1項</a> 支払いについて</strong></h3>
+<div style="margin-left: 1rem">
+	<a id="6-1-1" class="anchor"></a><a href="#6-1-1">1.</a>本サーバーでは<a href="?category=others&content=contactus">こちら</a>にて寄付を受け付けております。<br>
+		当サーバーの寄付受付はMojangの「Minecraftの商用利用に関するガイドライン(参考URL:<a href="https://account.mojang.com/terms#commercial">https://account.mojang.com/terms#commercial</a>)」の2020年12月現在の記載内容に基づいて実施しております。
+	<a id="6-1-2" class="anchor"></a><a href="#6-1-2">2.</a>プレイヤーは<a href="#6">本ルール第6条</a>の条項を理解したうえで支払いを行ったものとします。
+</div>
+<br>
+<h3 style="margin-left: 0.5rem"><strong><a id="6-2" class="anchor"></a><a href="#6-2">第2項</a> 払い戻しについて</strong></h3>
+<div style="margin-left: 1rem">
+	<a id="6-2-1" class="anchor"></a><a href="#6-2-1">1.</a>特別な理由を除き、払い戻しは受け付けられませんのでご了承ください。<br>
+	<a id="6-2-2" class="anchor"></a><a href="#6-2-2">2.</a>払い戻しを行う際は、振込手数料、事務手数料(通常1000円)、Vip権限の利用金額(Vip権限適用後から払い戻し申請日を期間とし、1日置きに15円として計算します。)を差し引いた額が1円を上回った場合とします。<br>
+	<a id="6-2-3" class="anchor"></a><a href="#6-2-3">3.</a>サービスを停止した後は払い戻しが受け付けられませんのでご了承ください。<br>
+	<a id="6-2-4" class="anchor"></a><a href="#6-2-4">4.</a>BAN処分をされた場合は払い戻しを受け付けられませんのでご了承ください。<br>
+</div>
+<h2 class="content-subtitle"><strong><a id="7" class="anchor"></a><a href="#7">第7条</a> 免責事項</strong></h2>
+<h3 style="margin-left: 0.5rem"><strong><a id="7-1" class="anchor"></a><a href="#7-1">第1項</a> プレイヤーの監視について</strong></h3>
+<div style="margin-left: 1rem">
+	<a id="7-1-1" class="anchor"></a><a href="#7-1-1">1.</a>プレイヤーの皆様に安心してプレイしていただけるよう、運営チームがゲーム内チャットメッセージや行動ログ等を確認する場合があります。<br>
+	<a id="7-1-2" class="anchor"></a><a href="#7-1-2">2.</a>処罰を実行する際に事実関係の確認のためゲーム内チャットメッセージや行動ログ等を確認する場合があります。<br>
+</div>
+<h3 style="margin-left: 0.5rem"><strong><a id="7-2" class="anchor"></a><a href="#7-2">第2項</a> トラブル、損失補填について</strong></h3>
+<div style="margin-left: 1rem">
+	<a id="7-2-1" class="anchor"></a><a href="#7-2-1">1.</a>プレイヤー同士のトラブルへは、ルール上処罰されるべき行為を確認できた場合を除き運営チームは一切関与しません。<br>プレイヤー間での解決に努めてください。<br>
+	<a id="7-2-2" class="anchor"></a><a href="#7-2-2">2.</a>プレイヤー間での取引に関しては自己の責任において行ってください。損失やトラブルが発生しても運営チームは一切関与、補填致しませんのでご了承ください。<br>
+	<a id="7-2-3" class="anchor"></a><a href="#7-2-3">3.</a>ルール違反による処置、プレイヤー間のトラブル、ゲーム内不具合、プレイヤーの過失によって発生した損失の補填はできませんのでご了承ください。<br>
+	<a id="7-2-4" class="anchor"></a><a href="#7-2-4">4.</a>サーバーメンテナンス時に強制的に切断されたことによる損失などの補填はできませんのでご了承ください。<br>
+	<a id="7-2-5" class="anchor"></a><a href="#7-2-5">5.</a>サーバーのロールバックが必要になる事象が発生した場合、ロールバックによって発生した損失などの補填はできませんのでご了承ください。<br>
+</div>
+<br>
+<time>2020年12月18日</time> 改定<br>
+<time>2020年9月10日</time> 改定
