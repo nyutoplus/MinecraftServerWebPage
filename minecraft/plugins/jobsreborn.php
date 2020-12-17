3e8f2594fe4597bc73e3d599f7cd2a12165411d3
nyutoplus <tttmasapi@gmail.com>
2020-12-17 15:06:27 +0900

    JobsRebornページの作成

--- minecraft/plugins/jobsreborn.php
+++ minecraft/plugins/jobsreborn.php
@@ -1,4 +1,3 @@
-
 <h1 class="content-title">
 	<a class="hide-line" href="https://www.spigotmc.org/resources/jobs-reborn.4216/">Jobs Reborn</a>の使用可能コマンド
 </h1>
@@ -10,5 +9,622 @@
 	</ul>
 	<br> <strong>[]</strong>で括ってある部分は省略可能です。<br> <strong>&lt;&gt;</strong>で括ってある部分は省略できないので引数を入力してください。<br> <strong>&lt;A|B|C&gt;</strong>で括ってある場合はA～Cのいずれかを入力してください。
 </div>
-<div style="height: 1rem"></div>
-<div>作成中・・・</div>
+<hr>
+<div>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobs" class="anchor"></a><a href="#jobs">/jobs</a><br>
+				<a href="#jobs">/jobs ? [ページ]</a><br>
+				<a href="#jobs">/jobs help [ページ]</a>
+			</h2>
+			<div class="mark-all">ALL</div>
+		</div>
+		<div class="cmdma">
+			Jobs Rebornのコマンドを表示します。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsjoin" class="anchor"></a><a href="#jobsjoin">/jobs join [職業]</a>
+			</h2>
+			<div class="mark-all">ALL</div>
+		</div>
+		<div class="cmdma">
+			指定した職業に就きます。<br>
+			職業を指定しない場合は専用のUIが開きます。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsitembonus" class="anchor"></a><a href="#jobsitembonus">/jobs itembonus</a>
+			</h2>
+			<div class="mark-all">ALL</div>
+		</div>
+		<div class="cmdma">
+			アイテムボーナスを表示します。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsbrowse" class="anchor"></a><a href="#jobsbrowse">/jobs browse</a>
+			</h2>
+			<div class="mark-all">ALL</div>
+		</div>
+		<div class="cmdma">
+			利用可能な職業の一覧を表示します。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsinfo" class="anchor"></a><a href="#jobsinfo">/jobs info [職業] [動作]</a>
+			</h2>
+			<div class="mark-all">ALL</div>
+		</div>
+		<div class="cmdma">
+			各職業のアクションでどれぐらい報酬が得られるかを表示します。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsbonus" class="anchor"></a><a href="#jobsbonus">/jobs bonus [職業]</a>
+			</h2>
+			<div class="mark-all">ALL</div>
+		</div>
+		<div class="cmdma">
+			指定した職業でどれぐらいボーナスが得られるか表示します。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsquests" class="anchor"></a><a href="#jobsquests">/jobs quests [プレイヤー名]</a>
+			</h2>
+			<div class="mark-all">ALL</div>
+		</div>
+		<div class="cmdma">
+			現在有効なクエストを表示します。<br>
+			<mark>(Admin_Lv1以上のみ)指定したプレイヤーのクエストを表示できます。</mark>
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsclearownership" class="anchor"></a><a href="#jobsclearownership">/jobs clearownership [プレイヤー名]</a>
+			</h2>
+			<div class="mark-all">ALL</div>
+		</div>
+		<div class="cmdma">
+			設置した設備の所有権を破棄します。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsstats" class="anchor"></a><a href="#jobsstats">/jobs stats [プレイヤー名]</a>
+			</h2>
+			<div class="mark-all">ALL</div>
+		</div>
+		<div class="cmdma">
+			就いている職業の情報を表示します。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobspoints" class="anchor"></a><a href="#jobspoints">/jobs points [プレイヤー名]</a>
+			</h2>
+			<div class="mark-all">ALL</div>
+		</div>
+		<div class="cmdma">
+			ポイントの情報を表示します。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobstop" class="anchor"></a><a href="#jobstop">/jobs top [職業]</a>
+			</h2>
+			<div class="mark-all">ALL</div>
+		</div>
+		<div class="cmdma">
+			指定した職業のトッププレイヤーを表示します。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsgtop" class="anchor"></a><a href="#jobsgtop">/jobs gtop</a>
+			</h2>
+			<div class="mark-all">ALL</div>
+		</div>
+		<div class="cmdma">
+			全職業合計のトッププレイヤーを表示します。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobslimit" class="anchor"></a><a href="#jobslimit">/jobs limit [プレイヤー名]</a>
+			</h2>
+			<div class="mark-all">ALL</div>
+		</div>
+		<div class="cmdma">
+			職業の支払い上限を表示します。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsskipquest" class="anchor"></a><a href="#jobsskipquest">/jobs skipquest [職業] [クエスト名] &lt;プレイヤー名&gt;</a>
+			</h2>
+			<div class="mark-all">ALL</div>
+		</div>
+		<div class="cmdma">
+			指定したクエストをスキップします。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsshop" class="anchor"></a><a href="#jobsshop">/jobs shop</a>
+			</h2>
+			<div class="mark-all">ALL</div>
+		</div>
+		<div class="cmdma">
+			ショップを開きます。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsleave" class="anchor"></a><a href="#jobsleave">/jobs leave [職業]</a>
+			</h2>
+			<div class="mark-all">ALL</div>
+		</div>
+		<div class="cmdma">
+			指定した職業から離職します。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsleaveall" class="anchor"></a><a href="#jobsleaveall">/jobs leaveall</a>
+			</h2>
+			<div class="mark-all">ALL</div>
+		</div>
+		<div class="cmdma">
+			全ての職業から離職します。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobstoggle" class="anchor"></a><a href="#jobstoggle">/jobs toggle &lt;actionbar | bossbar&gt;</a>
+			</h2>
+			<div class="mark-all">ALL</div>
+		</div>
+		<div class="cmdma">
+			Jobs Rebornのレベル、報酬表示を切り替えます。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobslog" class="anchor"></a><a href="#jobslog">/jobs log [プレイヤー名]</a>
+			</h2>
+			<div class="mark-all">ALL</div>
+		</div>
+		<div class="cmdma">
+			統計情報を表示します。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsarchive" class="anchor"></a><a href="#jobsarchive">/jobs archive [プレイヤー名]</a>
+			</h2>
+			<div class="mark-all">ALL</div>
+		</div>
+		<div class="cmdma">
+			離職中の職業を表示します。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsiteminfo" class="anchor"></a><a href="#jobsiteminfo">/jobs iteminfo</a>
+			</h2>
+			<div class="mark-all">ALL</div>
+		</div>
+		<div class="cmdma">
+			手に持っているアイテムの情報を表示します。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsblockinfo" class="anchor"></a><a href="#jobsblockinfo">/jobs blockinfo</a>
+			</h2>
+			<div class="mark-all">ALL</div>
+		</div>
+		<div class="cmdma">
+			見ているブロックの情報を表示します。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsarea" class="anchor"></a><a href="#jobsarea">/jobs area &lt;add | remove | info | list&gt;</a>
+			</h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div class="cmdma">
+			エリアを設定します。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobseditjobs" class="anchor"></a><a href="#jobseditjobs">/jobs editjobs</a>
+			</h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div class="cmdma">
+			各職業のアクションの報酬などを編集できます。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobseditpoints" class="anchor"></a><a href="#jobseditpoints">/jobs editpoints [set | add | take] [プレイヤー名] [量]</a>
+			</h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div class="cmdma">
+			ポイントを編集します。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsresetlimit" class="anchor"></a><a href="#jobsresetlimit">/jobs resetlimit [プレイヤー名]</a>
+			</h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div class="cmdma">
+			指定したプレイヤーの上限をリセットします。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsresetquest" class="anchor"></a><a href="#jobsresetquest">/jobs resetquest [プレイヤー名] [職業]</a>
+			</h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div class="cmdma">
+			指定したプレイヤーの指定した職業のクエストをリセットします。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobseditquest" class="anchor"></a><a href="#jobseditquest">/jobs editquest</a>
+			</h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div class="cmdma">
+			クエストを編集します。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsedititembonus" class="anchor"></a><a href="#jobsedititembonus">/jobs edititembonus &lt;list | add | remove&gt; [職業] [ブースト名]</a>
+			</h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div class="cmdma">
+			アイテムブーストボーナスを編集します。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobslevel" class="anchor"></a><a href="#jobslevel">/jobs level [プレイヤー名] [職業] [set | add | take] [量]</a>
+			</h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div class="cmdma">
+			指定したプレイヤーの職業のレベルを操作します。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsplayerinfo" class="anchor"></a><a href="#jobsplayerinfo">/jobs playerinfo [プレイヤー名] [職業] [動作]</a>
+			</h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div class="cmdma">
+			プレイヤーの情報を表示します。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsplaceholders" class="anchor"></a><a href="#jobsplaceholders">/jobs placeholders [パース] [プレースホルダー]</a>
+			</h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div class="cmdma">
+			各種情報を解析します。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsschedule" class="anchor"></a><a href="#jobsschedule">/jobs schedule enable [スケジュール名] [時間]</a>
+			</h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div class="cmdma">
+			スケジューラーを有効にします。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobstransfer" class="anchor"></a><a href="#jobstransfer">/jobs transfer [プレイヤー名] [現在の職業] [新しい職業]</a>
+			</h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div class="cmdma">
+			指定したプレイヤーの職業を移行します。<br>
+			※経験値はそのままです。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsexp" class="anchor"></a><a href="#jobsexp">/jobs exp [プレイヤー名] [職業] [set | add | take] [量]</a>
+			</h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div class="cmdma">
+			職業の経験値を操作します。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsexplored" class="anchor"></a><a href="#jobsexplored">/jobs explored</a>
+			</h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div class="cmdma">
+			現在立っているチャンクに来たことのある人を確認できます。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobspromote" class="anchor"></a><a href="#jobspromote">/jobs promote [プレイヤー名] [職業] [レベル]</a>
+			</h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div class="cmdma">
+			指定したプレイヤーの職業のレベルを上げます。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsdemote" class="anchor"></a><a href="#jobsdemote">/jobs demote [プレイヤー名] [職業] [レベル]</a>
+			</h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div class="cmdma">
+			指定したプレイヤーの職業のレベルを下げます。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsemploy" class="anchor"></a><a href="#jobsemploy">/jobs employ [プレイヤー名] [職業]</a>
+			</h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div class="cmdma">
+			指定したプレイヤーを指定した職業に就かせます。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsfire" class="anchor"></a><a href="#jobsfire">/jobs fire [プレイヤー名] [職業]</a>
+			</h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div class="cmdma">
+			指定したプレイヤーを指定した職業から離職させます。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsbp" class="anchor"></a><a href="#jobsbp">/jobs bp</a>
+			</h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div class="cmdma">
+			半径10ブロック以内の報酬を受け取れないブロックを表示します。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsfireall" class="anchor"></a><a href="#jobsfireall">/jobs fireall [&lt;プレイヤー名&gt; | all]</a>
+			</h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div class="cmdma">
+			指定したプレイヤーを全ての職業から離職させます。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsgrantxp" class="anchor"></a><a href="#jobsgrantxp">/jobs grantxp [プレイヤー名] [職業] [経験値]</a>
+			</h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div class="cmdma">
+			指定したプレイヤーの指定した職業に経験値を与えます。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsremovexp" class="anchor"></a><a href="#jobsremovexp">/jobs removexp [プレイヤー名] [職業] [経験値]</a>
+			</h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div class="cmdma">
+			指定したプレイヤーの指定した職業から経験値を取り除きます。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsexpboost" class="anchor"></a><a href="#jobsexpboost">/jobs expboost [&lt;職業&gt; | all | reset] [&lt;比率&gt; | all]</a>
+			</h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div class="cmdma">
+			指定した職業の経験値をブーストします。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobspointboost" class="anchor"></a><a href="#jobspointboost">/jobs pointboost [&lt;職業&gt; | all | reset] [&lt;比率&gt; | all]</a>
+			</h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div class="cmdma">
+			指定した職業のポイントをブーストします。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsmoneyboost" class="anchor"></a><a href="#jobsmoneyboost">/jobs moneyboost [&lt;職業&gt; | all | reset] [&lt;比率&gt; | all]</a>
+			</h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div class="cmdma">
+			指定した職業の収入をブーストします。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsgive" class="anchor"></a><a href="#jobsgive">/jobs give [プレイヤー名] [職業] [アイテム名 | 制限されたアイテム名] [職業のアイテム名]</a>
+			</h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div class="cmdma">
+			指定した職業のアイテムを取得します。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsconvert" class="anchor"></a><a href="#jobsconvert">/jobs convert</a>
+			</h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div class="cmdma">
+			データベースシステムを使っていないデータベースシステムにコンバートします。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobssignupdate" class="anchor"></a><a href="#jobssignupdate">/jobs signupdate [職業]</a>
+			</h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div class="cmdma">
+			手動で看板を更新します。
+		</div>
+	</div>
+	<hr>
+	<div class="cmdof">
+		<div class="cmdtt">
+			<h2 class="cmd">
+				<a id="jobsreload" class="anchor"></a><a href="#jobsreload">/jobs reload</a>
+			</h2>
+			<div class="mark-adminlv1">Admin_Lv1</div>
+		</div>
+		<div class="cmdma">
+			設定ファイルをリロードします。
+		</div>
+	</div>
+</div>
