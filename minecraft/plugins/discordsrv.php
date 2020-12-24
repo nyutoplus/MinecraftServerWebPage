f880952087cf5fd275102e381342c2c1be79d857
nyutoplus <tttmasapi@gmail.com>
2020-12-25 05:14:22 +0900

    誤字修正

--- minecraft/plugins/discordsrv.php
+++ minecraft/plugins/discordsrv.php
@@ -4,7 +4,7 @@
 <div style="padding-left: 1rem">
 	<ul>
 		<li>・MinecraftとDiscordで相互にチャットできるようにするプラグインです。</li>
-		<li>・オフライン時にプレイ中の人とチャットしたり、一々Minecraftの画面から離れてDiscdordを見に行く必要がなくなって便利</li>
+		<li>・オフライン時にプレイ中の人とチャットしたり、一々Minecraftの画面から離れてDiscordを見に行く必要がなくなって便利</li>
 	</ul>
 	<br> <strong>[]</strong>で括ってある部分は省略可能です。<br> <strong>&lt;&gt;</strong>で括ってある部分は省略できないので引数を入力してください。<br> <strong>&lt;A|B|C&gt;</strong>で括ってある場合はA～Cのいずれかを入力してください。
 </div>
